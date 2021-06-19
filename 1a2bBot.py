99d80d4356f26fdb359db9bb8d0243a402c36554
Hsuan Tsai <hsuantsai0114@gmail.com>
2021-06-19 19:54:44 +0800

    ver1

---
+++ 1a2bBot.py
@@ -0,0 +1,60 @@
+import random as rd
+import sys
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+token = '1889030668:AAEI-EWca833U-x8yFI7PRZIODGbPPYP01E'
+FILE_NAME = 'answer.txt'
+
+def myRandom() -> None:
+    real = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for i in range(0, 10):
+        k = rd.randint(0, 9)
+        real[i], real[k] = real[k], real[i]
+    f = open(FILE_NAME, 'w')
+    f.write('{:s}{:s}{:s}{:s}'.format(real[0], real[1], real[2], real[3]))
+    f.close()
+
+def myGuess(update: Update, context: CallbackContext) -> None:
+    counterA = 0
+    counterB = 0
+    f = open(FILE_NAME, 'r')
+    ans = f.read()
+    realNum = list(ans)
+    f.close()
+    
+    guess = list(update.message.text)
+    for i in range(0, 4):
+        if realNum[i]==guess[i]:
+            counterA+=1
+        for j in range(0, 4):
+            if realNum[i]==guess[j] and i!=j:
+                counterB+=1
+    update.message.reply_text('{:s}{:s}{:s}{:s}   {:d}A{:d}B'.format(guess[0], guess[1], guess[2], guess[3], counterA, counterB))
+
+    if counterA==4:
+        update.message.reply_text('You win!')
+        update.message.reply_text('Enter /start to start another game.')
+        return
+
+
+def startGame(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_markdown_v2(fr'Hi {user.mention_markdown_v2()}\!')
+    update.message.reply_text('Let the game start!')
+    myRandom()
+
+def main() -> None:
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', startGame))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, myGuess))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__=='__main__':
+    main()
+
+
+
---
+++ rand.py
@@ -0,0 +1,62 @@
+import random
+import sys
+
+FILE_NAME = 'answer.txt'
+
+def myRandom() -> None:
+    real = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for i in range(0, 10):
+        k = random.randint(0, 9)
+        tmp = real[i]
+        real[i] = real[k]
+        real[k] = tmp
+    f = open(FILE_NAME, 'w')
+    f.write('{:s}{:s}{:s}{:s}'.format(real[0], real[1], real[2], real[3]))
+    f.close()
+
+def myGuess(guess) -> None:
+    counterA = 0
+    counterB = 0
+    f = open(FILE_NAME, 'r')
+    ans = f.read()
+    realNum = list(ans)
+    f.close()
+
+    for i in range(0, 4):
+        if realNum[i]==guess[i]:
+            counterA+=1
+        for j in range(0, 4):
+            if realNum[i]==guess[j] and i!=j:
+                counterB+=1
+    print('{:s}{:s}{:s}{:s}   {:d}A{:d}B'.format(guess[0], guess[1], guess[2], guess[3], counterA, counterB))
+
+    if counterA==4:
+        print('You win!')
+        sys.exit(0)
+        return
+
+def main():
+    myRandom()
+    while True:
+        guess = input('Guess number: ')
+        guessList = list(guess)
+        myGuess(guessList)
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
