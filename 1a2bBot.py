b1473fb185415a0f4f057532be4c72b4a1b209fb
Hsuan Tsai <hsuantsai0114@gmail.com>
2021-07-07 11:21:47 +0800

    Add some informations.

--- 1a2bBot.py
+++ 1a2bBot.py
@@ -1,3 +1,8 @@
+# @File      : 1a2bBot.py
+# @Author     : Hsuan Tsai
+# @Date       : 2021-07-07
+# @Python Version: python 3.9
+
 import random as rd
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
